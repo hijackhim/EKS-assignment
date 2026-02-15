53b6925ba44ab24647ffef51c311666dc10bca9a
Himanshu Kumar Singh <105789918+hijackhim@users.noreply.github.com>
2026-02-16 02:27:57 +0530

    new

--- Terraform/nodegroup.tf
+++ Terraform/nodegroup.tf
@@ -1,20 +1,3 @@
-#module "eks_nodegroup" {
-#  source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
-#  version = "~>20.0"
-#
-#  cluster_name = module.eks.cluster_name
-#  subnet_ids   = module.vpc.private_subnets
-#  cluster_service_cidr = var.cluster_service_cidr
-#
-#  name = "private-ng"
-#
-# instance_types = ["t3.medium"]
-#
-#  desired_size = 3
-#  min_size     = 3
-#  max_size     = 3
-#}
-
 module "eks_nodegroup" {
   source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
   version = "~>20.0"
